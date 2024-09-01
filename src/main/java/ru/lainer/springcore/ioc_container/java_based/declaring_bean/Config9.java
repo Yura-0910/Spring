428291a32ea395fb1b7670f45365b8da84deb6cf
Yura Mazurov <mazurovyura09@yandex.ru>
2024-09-01 08:04:47 +0300

    Изменил пакет

--- src/main/java/ru/lainer/springcore/ioc_container/java_based/declaring_bean/Config9.java
+++ src/main/java/ru/lainer/springcore/ioc_container/java_based/declaring_bean/Config9.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("ru.lainer.springcore.ioc_container.java_based.declaring_bean.default_method")
+@ComponentScan("ru.lainer.springcore.ioc_container.java_based.declaring_bean")
 public class Config9 implements InterfaceWithBean{
 
 }

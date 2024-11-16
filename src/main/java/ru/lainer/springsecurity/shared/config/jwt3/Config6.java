77185904f7e878943f5ecc3c3830a3ca86e96f8b
Yura Mazurov <mazurovyura09@yandex.ru>
2024-11-16 17:37:54 +0300

    Поменял конфиг JWT: разрешил доступ к H2

--- src/main/java/ru/lainer/springsecurity/shared/config/jwt3/Config6.java
+++ src/main/java/ru/lainer/springsecurity/shared/config/jwt3/Config6.java
@@ -33,6 +33,7 @@ public class Config6 {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
             .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
             .anyRequest().authenticated())
         .addFilterBefore(FilterAuthJWT, UsernamePasswordAuthenticationFilter.class);

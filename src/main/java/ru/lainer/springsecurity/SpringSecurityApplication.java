5449f84f3cd29fe4ba5d7909a20dae8977a578e5
Yura Mazurov <mazurovyura09@yandex.ru>
2024-11-18 18:02:33 +0300

    Обновил TODO

--- src/main/java/ru/lainer/springsecurity/SpringSecurityApplication.java
+++ src/main/java/ru/lainer/springsecurity/SpringSecurityApplication.java
@@ -10,16 +10,11 @@ public class SpringSecurityApplication {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
 
-	//TODO попробовать использовать базу H2
-	//TODO для H2 нужен драйвер нужной версии
-	//TODO развернуть БД Postgress + backEnd на виртуальной машине (Cloud.ru) ?
-	//TODO Описать про профили и про запросы для каждого профиля в ReadMe.md
-	//TODO создать тесты
 	//TODO прокоментировать весь код (в стиле Java Doc) + ReadMe.md
-	//TODO из ServiceSignUp вынести общую функциональность касающяюся заполнения MyUSER ролями
-	//TODO общую функциональность из ServiceSignUpJwt и ServiceSignUp выделить в
-	// отдельный класс, или отрефакторить эти сервисы
-	//TODO Refresh tocken, кэш:: добавить в issue проекта Spring Security на git.hub
-	//TODO код для Bearer и для JWT
+	//TODO Описать про профили и про запросы для каждого профиля в ReadMe.md
 
+	//TODO общую функциональность касающуюся заполнения MyUSER (из ServiceSignUpJwt и ServiceSignUp)
+	// вынести в отдельный класс\метод (рефакторинг)
+
+	//TODO для H2 нужен драйвер нужной версии (как в DBeaver, так и в IDEA)
 }
